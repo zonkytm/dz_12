73b422146da43e69fcf55d51994ce60cf5424041
zonky <Juphy@yandex.ru>
2022-04-20 13:36:56 +0300

    Добавьте файлы проекта.

---
+++ dz_12/Abstract.h
@@ -0,0 +1,70 @@
+#ifndef abstract_h
+
+#define abstract_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Animal {
+protected:
+	int age;
+	string color;
+public:
+	string sort;
+	Animal(int age, string color) {
+		this->age = age;
+		this->color = color;
+
+	}
+	virtual void voice() = 0;
+	
+
+};
+
+
+
+class Parrot :public Animal {
+public:
+	
+	Parrot(int age, string color) :Animal(age, color) {
+		
+		this->sort = "Parrot";
+	}
+	void voice() override {
+		cout << "sing\n";
+	}
+};
+
+
+class Dog :public Animal {
+public:
+	Dog(int age, string color) :Animal(age, color) {
+		
+		this->sort = "Dog";
+	}
+	void voice() override {
+		cout << "Gawg\n";
+	}
+};
+
+
+class Cat :public Animal {
+public:
+	Cat(int age, string color) :Animal(age, color) {
+		
+		this->sort = "Cat";
+	}
+	void voice() override {
+		cout << "Meow\n";
+	}
+};
+
+
+
+
+
+
+#endif
---
+++ dz_12/dz_12.cpp
@@ -0,0 +1,41 @@
+﻿#include "Abstract.h"
+#include <algorithm>
+#include <vector>
+
+int main() {
+	int size;
+	cin >> size;
+	vector<Animal*> zoo;
+	for (int i = 0; i < size; i++)
+	{
+		int g = rand() % 3 + 1;
+		switch (g)
+		{
+		case 1:
+			zoo.push_back(new Parrot(rand()%12+1,"Red"));
+		case 2:
+			zoo.push_back(new Cat(rand() % 12 + 1, "Black"));
+		case 3:
+			zoo.push_back(new Dog(rand() % 12 + 1, "white"));
+		default:
+			break;
+		}
+	}
+
+
+	auto newEnd = remove_if(zoo.begin(), zoo.end(), [](Animal* animal) {return animal->sort == "Dog";});
+
+	for (int i = 0; i < zoo.size(); i++)
+	{
+		cout << zoo[i]->sort << endl;
+	}
+	cout << endl << endl;
+	// Удаляем все элементы, следующие после
+	zoo.erase(newEnd, zoo.end());
+
+
+	for (int i = 0; i < zoo.size(); i++)
+	{
+		cout << zoo[i]->sort << endl;
+	}
+}
